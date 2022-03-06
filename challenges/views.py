a7827134f402fefe4fc1a44aa07f3592c8fa1bc1
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-06 15:38:36 +0330

    Add dynamic views and urls to handle alphanumeric urls.
    * Use path converters in urls.
    * Create different views for handling numeric and alphabetical requests.
    * Redirect to alphabetical paths when using numeric paths.

--- challenges/views.py
+++ challenges/views.py
@@ -1,19 +1,50 @@
 from django.http import HttpRequest, HttpResponse
-from django.http.response import HttpResponseNotFound
+from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 
+monthly_challenges = {
+    "january": "Code every day",
+    "february": "Prepare for a long valentine day",
+    "march": "Learn basics of hacking just for fun",
+    "april": "Learn C++ programming language",
+    "may": "Learn Golang",
+    "june": "Go to France",
+    "july": "Learn 1500 new French words",
+    "august": "Get deep into hacking",
+    "september": "Celebrate your existence",
+    "october": "Master TypeScript",
+    "november": "Build a better website for yourself with NextJS",
+    "december": "Master Django"
+}
+
+def numerical_monthly_challenge(_: HttpRequest, month: int):
+    """
+    Redirects to monthly_challenge if the number is between 1 and 12, otherwise
+    sends a http not found response
+    """
+    months = [
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "october",
+        "november",
+        "december"
+    ]
+    if 1 <= month <= 12:
+        redirect_month = months[month - 1]
+    else:
+        return HttpResponseNotFound("Invalid month")
+    return HttpResponseRedirect("/challenges/{path}".format(path=redirect_month))
 
 def monthly_challenge(_: HttpRequest, month: str):
     """Sends the apopriate response for monthly challenges request"""
-
-    challenge_text = ""
-    month = month.lower()
-    if month == "january":
-        challenge_text = "Code every day"
-    elif month == "february":
-        challenge_text = "Prepare for a long valentine day"
-    elif month == "march":
-        challenge_text = "Learn basics of hacking just for fun"
-    else:
+    try:
+        challenge_text = monthly_challenges[month.lower()]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This month is not supported")
-
-    return HttpResponse(challenge_text)

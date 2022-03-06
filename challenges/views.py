8ea798e80e43149a6bd132f4b30d17eb76235881
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-06 12:13:52 +0330

    Add a new view and url for challenges/january/ path
    * Create the index view in challenges/views.py
    * Add a new url to challenges/urls.py and use the index view.
    * Include the newly created urls.py to monthly_challenges/urls file.

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def index(_: HttpRequest):
+    return HttpResponse("Hello from Django")

e06f2ded9283e38eacfd17a7da00c1013583316b
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-06 12:49:51 +0330

    Add a dynamic view for all months and replace old view functions with it.

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpRequest, HttpResponse
+from django.http.response import HttpResponseNotFound
 
-# Create your views here.
-def january(_: HttpRequest):
-    return HttpResponse("Code every day")
 
-def february(_: HttpRequest):
-    return HttpResponse("Prepare for a long valentine day")
+def monthly_challenge(_: HttpRequest, month: str):
+    """Sends the apopriate response for monthly challenges request"""
+
+    challenge_text = ""
+    month = month.lower()
+    if month == "january":
+        challenge_text = "Code every day"
+    elif month == "february":
+        challenge_text = "Prepare for a long valentine day"
+    elif month == "march":
+        challenge_text = "Learn basics of hacking just for fun"
+    else:
+        return HttpResponseNotFound("This month is not supported")
+
+    return HttpResponse(challenge_text)

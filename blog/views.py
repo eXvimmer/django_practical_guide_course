3f2d524ddd05e6ab94c13bdb8201cece8c333d45
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-11 22:43:04 +0330

    Add initial urls for the app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
+
+
+def starting_page(_: HttpRequest):
+    pass
+
+
+def posts(_: HttpRequest):
+    pass
+
+
+def post_detail(_: HttpRequest):
+    pass

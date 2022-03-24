4cd76d6180b561e31699810496b7f939cd412d11
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-25 01:12:58 +0430

    Add a slug field, overwrite the save method and migrate again.

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Book(models.Model):
@@ -10,9 +11,14 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_bestseller = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])  # type: ignore
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)

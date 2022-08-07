b78cd74ed6f972694945aeb15067a4aa04568f71
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-08 02:14:48 +0430

    Add a one-to-many relationship between book and author models and migrate.

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -3,13 +3,20 @@ from django.db import models
 from django.urls import reverse
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(null=True, max_length=100)
-    is_bestseller = models.BooleanField(default=False)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name="author", null=True
+    )
+    is_bestseller = models.BooleanField(default=False)  # type: ignore
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
     def __str__(self):

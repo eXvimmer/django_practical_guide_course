99e630682cca0db227f3359b307ee2defc0b7723
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-09 01:30:26 +0430

    Create models for Posts, Authors and Tags.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=32)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email_address = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    excerpt = models.CharField(max_length=255)
+    # TODO: change image_name
+    image_name = models.CharField(max_length=128)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(allow_unicode=True, unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)  # null=True?

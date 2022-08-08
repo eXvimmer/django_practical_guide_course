997e4bbf22cec7c2aa6540dcb4ff529cd91d4cb0
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-09 01:39:36 +0430

    Register models to site admin and make migrations.

--- blog/models.py
+++ blog/models.py
@@ -20,5 +20,7 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(allow_unicode=True, unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, related_name="posts", null=True
+    )
     tags = models.ManyToManyField(Tag)  # null=True?

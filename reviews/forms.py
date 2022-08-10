11f8ab4ba22a3cba9795ba0c9bb70d6a0d8abf4a
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-10 14:27:55 +0430

    Use modelForm to create a form based on a model.

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,16 +1,10 @@
 from django import forms
+from .models import Review
 
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(
-        label="Your Name",
-        max_length=100,
-        error_messages={
-            "required": "name cannot be empty",
-            "max_length": "name should be at most 100 characters long",
-        },
-    )
-    review_text = forms.CharField(
-        label="Your Feedback", widget=forms.Textarea, max_length=255
-    )
-    rating = forms.IntegerField(min_value=1, max_value=5, label="Your Rating")
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        # fields = ["user_name", "review_text", "rating"]
+        # exclude = ["secret_field"]
+        fields = "__all__"

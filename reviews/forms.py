2f2845590b16b58494779be84416715e98dc6455
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-10 14:33:44 +0430

    Configure the model form.

--- reviews/forms.py
+++ reviews/forms.py
@@ -8,3 +8,14 @@ class ReviewForm(forms.ModelForm):
         # fields = ["user_name", "review_text", "rating"]
         # exclude = ["secret_field"]
         fields = "__all__"
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating",
+        }
+        error_messages = {
+            "user_name": {
+                "required": "name cannot be empty",
+                "max_length": "please enter a shorter name",
+            }
+        }

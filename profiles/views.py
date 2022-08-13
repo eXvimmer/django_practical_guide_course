839600f614fbbe004ced8f13cb084ec1ef72dbdc
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-13 11:36:43 +0430

    Inherit from CreateView instead of View.

--- profiles/views.py
+++ profiles/views.py
@@ -1,21 +1,10 @@
-from django.http import HttpRequest, HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import CreateView
 
-from .forms import ProfileForm
 from .models import UserProfile
 
 
-class CreateProfileView(View):
-    def get(self, request: HttpRequest):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {"form": form})
-
-    def post(self, request: HttpRequest):
-        submitted_form = ProfileForm(request.POST, request.FILES)
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-
-        return render(request, "profiles/create_profile.html", {"form": submitted_form})
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"

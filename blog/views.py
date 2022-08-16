555a54aca7b6d0446456eb7891a270c5ceb89125
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-16 09:10:44 +0430

    Remove an article from read later list.

--- blog/views.py
+++ blog/views.py
@@ -26,13 +26,25 @@ class PostsView(ListView):
 
 
 class PostDetailView(View):
+    def is_stored_post(self, request: HttpRequest, post_id: int):
+        stored_posts = request.session.get("stored_posts")  # type: ignore
+
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request: HttpRequest, slug: str):
         post = Post.objects.get(slug=slug)  # type: ignore
+
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
             "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id),
         }
         return render(request, "blog/post-detail.html", context=context)
 
@@ -51,6 +63,7 @@ class PostDetailView(View):
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
             "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id),
         }
         return render(request, "blog/post-detail.html", context=context)
 
@@ -80,6 +93,8 @@ class ReadLaterView(View):
 
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.remove(post_id)
 
+        request.session["stored_posts"] = stored_posts
         return HttpResponseRedirect("/")
